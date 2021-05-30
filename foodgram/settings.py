aff4d8dedce61bcb68df548ab4eeae5b192cf2a6
RadikKhabibulin <radikkhabibulin.ya@yandex.ru>
2021-05-31 02:48:25 +0300

    changed nginx

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -123,7 +123,7 @@ LOGOUT_REDIRECT_URL = 'index'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL_G')
 EMAIL_HOST = os.environ.get('EMAIL_HOST_G')
 EMAIL_PORT = os.environ.get('EMAIL_PORT_G')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER_G')
