cd3c04610b426f3c74e2f420b3c6c949a7b6d3f3
RadikKhabibulin <radikkhabibulin.ya@yandex.ru>
2021-05-30 17:29:44 +0300

    domain in index

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.shortcuts import (get_list_or_404, get_object_or_404, redirect,
                               render)
+from django.contrib.sites.shortcuts import get_current_site
 
 from foodgram.settings import PAGE_SIZE
 from .forms import RecipeForm
@@ -17,7 +18,9 @@ def index(request, tags='empty'):
     paginator = Paginator(recipes, PAGE_SIZE)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
+    domain = get_current_site(request)
     return render(request, 'index.html', {
+            'domain': domain,
             'page': page,
             'tags': tags, 'paginator': paginator,
         }
